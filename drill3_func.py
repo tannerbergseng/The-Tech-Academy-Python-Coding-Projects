56345f4a04d74674965e2b472e1b2a08a97a3abf
tannerbergseng <50608517+tannerbergseng@users.noreply.github.com>
2019-08-12 13:43:08 -0700

    Add files via upload

---
+++ drill3_func.py
@@ -0,0 +1,66 @@
+
+
+
+from tkinter import *
+import tkinter as tk
+
+import drill3
+import drill3_gui
+from tkinter import messagebox
+import os
+from tkinter import filedialog
+import shutil
+root = Tk()
+
+
+
+def center_window(self, w, h): # pass in the tkinter frame (master) reference and the w and h
+    # get user's screen width and height
+    screen_width = self.master.winfo_screenwidth()
+    screen_height = self.master.winfo_screenheight()
+    # calculate x and y coordinates to paint the app centered on the user's screen
+    x = int((screen_width/2) - (w/2))
+    y = int((screen_height/2) - (h/2))
+    centerGeo = self.master.geometry('{}x{}+{}+{}'.format(w, h, x, y))
+    return centerGeo
+
+
+
+
+# catch if the user's clicks on the windows upper-right 'X' to ensure they want to close
+def ask_quit(self):
+    if messagebox.askokcancel("Exit program", "Okay to exit application?"):
+        # This closes app
+        self.master.destroy()
+        os._exit(0)
+
+def browser(self):
+    """Browse file directory."""
+    path = filedialog.askdirectory()
+    print (self.txt_browse1.insert(INSERT,path))
+
+
+def browser2(self):
+    """Browse file directory."""
+    path = filedialog.askdirectory()
+    print (self.txt_browse2.insert(INSERT,path))
+
+
+def checkfile(self):
+    source = os.listdir(self.txt_browse1)
+    destination = self.txt_browse2
+    for files in source:
+        if files.endswith(".txt"):
+            shutil.copy(files,destination)
+
+
+
+
+
+
+   
+ 
+
+
+if __name__ == "__main__":
+    pass
---
+++ drill3_gui.py
@@ -0,0 +1,45 @@
+
+
+
+
+from tkinter import *
+import tkinter as tk
+
+import drill3
+import drill3_func
+from tkinter import messagebox
+import os
+from tkinter import filedialog
+import shutil
+root = Tk()
+
+def load_gui(self):
+
+    # Text Box
+    self.txt_browse1 = tk.Entry(self.master,text='',width=70)
+    self.txt_browse1.grid(row=0,column=2,rowspan=1,columnspan=1,padx=(30,40),pady=(40,0),sticky=N+W)
+    self.txt_browse2 = tk.Entry(self.master,text='',width=70)
+    self.txt_browse2.grid(row=1,column=2,rowspan=1,columnspan=1,padx=(30,40),pady=(0,0),sticky=N+W)
+
+
+    # Buttons
+    self.btn_add = tk.Button(self.master,width=12,height=1,text='Browse..',command=lambda: drill3_func.browser(self))
+    self.btn_add.grid(row=0,column=1,padx=(5,0),pady=(40,0),sticky=W)
+    self.btn_add = tk.Button(self.master,width=12,height=1,text='Browse..',command=lambda: drill3_func.browser2(self))
+    self.btn_add.grid(row=1,column=1,padx=(5,0),pady=(5,0),sticky=W)
+    self.btn_update = tk.Button(self.master,width=12,height=2,text='Check for files...', command=lambda:drill3_func.checkfile(self))
+    self.btn_update.grid(row=2,column=1,padx=(5,0),pady=(5,0),sticky=W)
+    self.btn_update = tk.Button(self.master,width=12,height=2,text='Close Program',command=lambda: drill3_func.ask_quit(self))
+    self.btn_update.grid(row=2,column=2,padx=(5,0),pady=(5,0),sticky=E)
+    
+
+    
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
